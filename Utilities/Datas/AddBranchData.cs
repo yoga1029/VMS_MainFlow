fe7b2d2d7fc569d9ab5e959f9eaa518647d11deb
Yogeswari S <yogeswari@riota.in>
2026-02-14 11:15:14 +0530

    .

--- Utilities/Datas/AddBranchData.cs
+++ Utilities/Datas/AddBranchData.cs
@@ -8,39 +8,39 @@ namespace VMS_MainFlow.Utilities.Datas
 {
     internal class AddBranchData
     {
-        public static Dictionary<string, string> addBranchSuccess = new Dictionary<string, string>
-        {
-            {"searchName", "AutoTest Branch"},
-            { "name", "AutoTest Branch" },
-            { "location", "Chennai" },
-            { "code", "011" },
-            { "email", "autobranch1@gmail.com"},
-            { "address", "AMA Nagar, Nizam Colony, Pudukkottai, 612001" },
-            { "contactDetails",  "9159007718" },
-            { "gstNo","ABCKKFGH12345IJ" },
-            { "companyName", "No0olMoxhi"},
-            { "companyContactNo", "9081040521" },
-            { "companyEmail", "noolmg0hhi@gmail.com" },
-            { "companyAddress", "AMA Nagar, Nizam Colony, Pudukkottai, 612001" },
-            { "fileUpload", "images.jpg" }
-        };
-
         //public static Dictionary<string, string> addBranchSuccess = new Dictionary<string, string>
         //{
-        //    {"searchName", "Pudukkottai Branch"},
-        //    { "name", "Pudukkottai Branch" },
-        //    { "location", "Pudukkottai" },
-        //    { "code", "101" },
-        //    { "email", "pudukkottaibranch@gmail.com"},
-        //    { "address", "AMA Nagar, Nizam Colony, Pudukkottai, 622001" },
-        //    { "contactDetails",  "9159617718" },
-        //    { "gstNo","ABCDEFGH12345IJ" },
-        //    { "companyName", "NoolMozhi"},
-        //    { "companyContactNo", "9080040521" },
-        //    { "companyEmail", "noolmozhi@gmail.com" },
-        //    { "companyAddress", "AMA Nagar, Nizam Colony, Pudukkottai, 622001" },
-        //    { "fileUpload", @"C:\Users\Yogeswari-PC\Pictures\F1.png" }
+        //    {"searchName", "AutoTest Branch"},
+        //    { "name", "AutoTest Branch" },
+        //    { "location", "Chennai" },
+        //    { "code", "011" },
+        //    { "email", "autobranch1@gmail.com"},
+        //    { "address", "AMA Nagar, Nizam Colony, Pudukkottai, 612001" },
+        //    { "contactDetails",  "9159007718" },
+        //    { "gstNo","ABCKKFGH12345IJ" },
+        //    { "companyName", "No0olMoxhi"},
+        //    { "companyContactNo", "9081040521" },
+        //    { "companyEmail", "noolmg0hhi@gmail.com" },
+        //    { "companyAddress", "AMA Nagar, Nizam Colony, Pudukkottai, 612001" },
+        //    { "fileUpload", "images.jpg" }
         //};
+
+        public static Dictionary<string, string> addBranchSuccess = new Dictionary<string, string>
+        {
+            {"searchName", "Pudukkottai Branch"},
+            { "name", "Pudukkottai Branch" },
+            { "location", "Pudukkottai" },
+            { "code", "101" },
+            { "email", "pudukkottaibranch@gmail.com"},
+            { "address", "AMA Nagar, Nizam Colony, Pudukkottai, 622001" },
+            { "contactDetails",  "9159617718" },
+            { "gstNo","ABCDEFGH12345IJ" },
+            { "companyName", "NoolMozhi"},
+            { "companyContactNo", "9080040521" },
+            { "companyEmail", "noolmozhi@gmail.com" },
+            { "companyAddress", "AMA Nagar, Nizam Colony, Pudukkottai, 622001" },
+            { "fileUpload", @"C:\Users\Yogeswari-PC\Pictures\F1.png" }
+        };
 
 
     }
