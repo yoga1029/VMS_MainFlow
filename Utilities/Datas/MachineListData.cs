95dbf59a4d3c33cf8263dafd4f28511a0d2d9d43
Yogeswari S <yogeswari@riota.in>
2026-02-17 15:42:48 +0530

    .

--- Utilities/Datas/MachineListData.cs
+++ Utilities/Datas/MachineListData.cs
@@ -149,7 +149,7 @@ namespace VMS_MainFlow.Utilities.Datas
         {
             { //Four Functions
   
-                "Banglore","thisStreet to thatStreet"
+                "Mangalore","isStreet to atStreet"
             },
 
         };
